0e5a5e5ea00a9f3f022cbd28605404c0d428a8bb
Damian de Hoog <37507522+DdeHoog@users.noreply.github.com>
2025-04-01 22:43:25 +0200

    Added functionality for destroying enemies alongside renaming

--- Enemy.gd
+++ Enemy.gd
@@ -1,4 +1,9 @@
 extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
+	#Allows for destruction of enemy by jumping on em
+	if body.velocity.y > 0 and body.global_position.y < global_position.y:
+			body.velocity.y = -500
+			queue_free()
+			return
 	get_tree().quit()
