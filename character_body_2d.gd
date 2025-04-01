d96e8ee38db9f84e03e3a540cce214d30b546727
Damian de Hoog <37507522+DdeHoog@users.noreply.github.com>
2025-04-01 20:34:14 +0200

    Added enemy entity
    Added 3 enemy entities, created collision for the player. If player touches, quit the testRun

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 500.0
-const JUMP_VELOCITY = -500.0
+const JUMP_VELOCITY = -600.0
 const FRIC = 50
 
 # Get gravity from the project settings to bve synced with RigidBody nodes.
@@ -15,12 +15,12 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("Left", "Right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
