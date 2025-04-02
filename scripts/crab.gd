5042c51b12db62eb23c79645e9253fa846c3b735
Damian de Hoog <37507522+DdeHoog@users.noreply.github.com>
2025-04-02 14:22:41 +0200

    Added check to see if game is running before player movement

--- scripts/crab.gd
+++ scripts/crab.gd
@@ -8,15 +8,18 @@ const JUMP_VELOCITY = -1750
 func _physics_process(delta):
 	velocity.y += GRAVITY * delta
 	if is_on_floor():
-		$RunCol.disabled = false
-		if Input.is_action_pressed("Jump"):
-			velocity.y = JUMP_VELOCITY
-			$JumpSound.play()
-		elif Input.is_action_pressed("Down"):
-			$AnimatedSprite2D.play("Duck")
-			$RunCol.disabled = true
-		else:
-			$AnimatedSprite2D.play("Run")
+		if not get_parent().game_running: #get func from parent node(main.scn) to check if game started.
+			$AnimatedSprite2D.play("Idle")
+		else: #if game started:
+			$RunCol.disabled = false
+			if Input.is_action_pressed("Jump"):
+				velocity.y = JUMP_VELOCITY
+				$JumpSound.play()
+			elif Input.is_action_pressed("Down"):
+				$AnimatedSprite2D.play("Duck")
+				$RunCol.disabled = true
+			else:
+				$AnimatedSprite2D.play("Run")
 	else:
 		$AnimatedSprite2D.play("Jump")
 	#Moves the body based on velocity. If the body collides with another, 
