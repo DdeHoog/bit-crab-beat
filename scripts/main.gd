8334ee86994a988ff13087b9a2e6ce0a12867ed3
Damian de Hoog <37507522+DdeHoog@users.noreply.github.com>
2025-04-07 14:34:59 +0200

    Added main menu with controls
    Main menu is default start place.
    Controls menu added.
    start menu start, then prompt player for input to start running.

--- scripts/main.gd
+++ scripts/main.gd
@@ -12,7 +12,6 @@ var speed : int #allows for playerspeed to vary based on lvl/score/time
 var screen_size : Vector2i #prep variable for screensize
 var score : int #variable to keep track of score
 var game_running : bool #boolean to see if game is running or not
-var beat_trigger : bool
 var inside_good_hitbox := false #Vars to check which hitbox the player box is inside of
 var inside_perfect_hitbox := false
 var can_score_good := true
@@ -20,41 +19,58 @@ var can_score_perfect := true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	screen_size = get_window().size #get the window size, for ground scrolling
-	$GameOver.get_node("RestartButton").pressed.connect(new_game)#when button pressed, new game trigger
-	new_game()#init new game
+	screen_size = get_window().size #get the window size
+	main_menu()
+	
+func main_menu():
+	$HUD.hide()
+	$Crab.hide()
+	$GameOver.hide()
+	$HUD/StartLabel.hide()
+	check_high_score()
+	get_tree().paused = true
+	game_running = false
+	$MainMenu.show()
+	$MainMenu.get_node("StartButton").pressed.connect(new_game)#Start game
 	
 	
-func new_game():#called upon each new game start
-	#reset variables
+func game_over():
+	check_high_score()#puts new highscore on the screen
+	get_tree().paused = true
+	game_running = false
+	$GameOver.show()
+	$GameOver.get_node("RestartButton").pressed.connect(new_game)#Restart trigger
+	
+func new_game():
+	#Reset variables
 	score = 0 #set score back to 0 for the new game
 	show_score() #displays score even before game start
 	can_score_good = true # Reset flag
 	can_score_perfect = true # Reset flag
 	game_running = false #prevent game autostarting
-	get_tree().paused = false #unpauses game after gameover
-	beat_trigger = false
+	get_tree().paused = false #unpauses game after gameover or gamestart
+	
+	#To reset player & camera
 	CRAB_START_POS = Vector2i((screen_size.x/4), 485) # Set crab start position at 1/4th of the screen x width
 	CAM_START_POS = get_window().size/2 #set camera deadcenter of the gamewindow size
-	
-	#To reset all the nodes back to the start
 	$Crab.position = CRAB_START_POS #reset crab to starting position
 	$Camera2D.position = CAM_START_POS #reset cam back to start position
 	
 	#reset hud
-	$HUD.get_node("StartLabel").show()
+	$MainMenu.hide()
 	$GameOver.hide()
+	$HUD.show()
+	$HUD.get_node("StartLabel").show()
+	$Crab.show()
 	$HUD.get_node("BonusScoreLabel").hide()
 	$HUD.get_node("GoodHitboxLabel").hide()
 	$HUD.get_node("PerfectHitboxLabel").hide()
-	
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if game_running:#if boolean is true from player input, run game.
 		$Crab.position.x = ($Conductor.song_position * START_SPEED) + CRAB_START_POS.x
 		$Camera2D.position.x = ($Conductor.song_position * START_SPEED) + CAM_START_POS.x	
-		#Display updated score, its updated based on beat in _on
 		show_score()
 	else:
 		if Input.is_action_pressed("Start"):#if game not running wait for player input.
@@ -92,11 +108,18 @@ func show_score():
 	#Get the scorelabel from the hud scene, 
 	$HUD.get_node("ScoreLabel").text = "SCORE: " + str(score)
 	
-	
 func check_high_score():
 	if score > high_score:
-		high_score == score
+		high_score = score
 		$HUD.get_node("HighscoreLabel").text = "SCORE: " + str(high_score)
+		
+func good_score():
+	score += 30
+	#print_debug(score)
+	
+func perfect_score():
+	score += 50
+	#print_debug(score)
 	
 func temporary_label(label_node, duration: float):
 	label_node.show()
@@ -108,20 +131,7 @@ func _on_conductor_beat_in_song(position):
 			score +=10
 			temporary_label($HUD.get_node("BonusScoreLabel"), 1.5)
 	score += 1
-
-func game_over():#setup for gameover condition, need collision and player hp to use this
-	check_high_score()#puts new highscore on the screen
-	get_tree().paused = true
-	game_running = false
-	$GameOver.show()
 	
-func good_score():
-	score += 30
-	#print_debug(score)
-	
-func perfect_score():
-	score += 50
-	#print_debug(score)
 	
 #Functions to check inside which hitbox we are, the hitboxes are split over 2 area shapes.
 func _on_arrow_up_good_body_entered(body):
