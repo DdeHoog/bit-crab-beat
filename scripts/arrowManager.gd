fe357bae4b03928d8cac6cbd207e0c4a1a6fc920
Naga <prodbynaga@gmail.com>
2025-04-08 12:34:19 -0300

    LEVEL AFFFFFFF

--- scripts/arrowManager.gd
+++ scripts/arrowManager.gd
@@ -11,12 +11,12 @@ func _ready():
 	arrayID = 0;
 	
 #Called every frame 'delta' is elapsed time since the previous frame.
-func _process(delta):
-	if (((crabObject.position.x + 500) > arrowArray[arrayID].position.x) && arrayID < arrowArray.size()):
+#func _process(delta):
+	#if (((crabObject.position.x + 500) > arrowArray[arrayID].position.x) && arrayID < arrowArray.size()):
 		#print_debug("test");
-			arrowArray[arrayID].visible = true;
-			arrayID = arrayID+1;
-	if (((crabObject.position.x + 1000) > arrowArray[arrayCloseID].position.x) && arrayCloseID < arrowArray.size()):
+			#arrowArray[arrayID].visible = true;
+			#arrayID = arrayID+1;
+	#if (((crabObject.position.x + 1000) > arrowArray[arrayCloseID].position.x) && arrayCloseID < arrowArray.size()):
 		#print_debug("test");
-			arrowArray[arrayCloseID].visible = false;
-			arrayCloseID = arrayCloseID+1;
+			#arrowArray[arrayCloseID].visible = false;
+			#arrayCloseID = arrayCloseID+1;
